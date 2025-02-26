1d7e64e518cf09cfd300aaea9f0a1196dba3d3e6
Nikita Filonov <nikita.filonov@dif.tech>
2025-02-26 23:35:27 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,31 +1,18 @@
-"""
-Форматы представления данных, JSON, YAML, XML/SOAP
-Знакомство с pydantic
-Знакомство с pydantic-settings
-Написание API клиентов
-Знакомстов с Swagger
-Знакомство с протоколами
-HTTP протокол, коды ответов
-gRPC пртокол, прото контракты
-WebSocket
-TCP/IP протоколы
-Про авторизацию а утентификацию, идентификацию
-Валидация схемы
-Покрытие API автотестами, можно разобрать ту библиотеку, которую сделал артем ерошенко
-Про взаимодействие с базой данных, про raw sql, про sqlalchemy
-
-TODO может не обязательно это куда-то деплоить? Можно и настроить локально, нужно подумать
-"""
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
 
 from apps.courses.api import courses_app_router
 from apps.exercises.api import exercises_app_router
+from apps.files.api import files_app_router
 from apps.users.api import users_app_router
 from utils.clients.database.engine import create_database
 
 app = FastAPI(title="QA Automation engineer course API")
 
+app.mount("/static", StaticFiles(directory="storage"), name="static")
+
 app.include_router(users_app_router, prefix="/api/v1")
+app.include_router(files_app_router, prefix="/api/v1")
 app.include_router(courses_app_router, prefix="/api/v1")
 app.include_router(exercises_app_router, prefix="/api/v1")
 
