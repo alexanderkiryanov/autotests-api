cc47b67878deafb517e3b556412ef48d4ce8a430
Nikita Filonov <nikita.filonov@dif.tech>
2025-03-16 20:19:10 +0300

    Initial commit

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
+        extra='allow',
         env_file='.env',
         env_file_encoding='utf-8',
         env_nested_delimiter='.',
